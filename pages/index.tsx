c2895d89a9e0e82f9a3da7083c8088ca85f8169c
Syntax365 <syntax365@gmail.com>
2022-10-30 15:35:09 -0400

    Revert "Migrate to nextJS App Dir with RFC"
    This reverts commit b454bb05c2d554f9b803ab013c49dd74eb482eef.

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tyler Prill</title>
+      </Head>
+
+      <main className={styles.main}>
+        <motion.h1
+          className={"text-3xl font-bold "}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.7,
+            delay: 0.4,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          Tyler Prill
+        </motion.h1>
+        <motion.p
+          className={"text-2xl font-bold"}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.7,
+            delay: 0.8,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          Software Engineer
+        </motion.p>
+      </main>
+    </div>
+  );
+}
