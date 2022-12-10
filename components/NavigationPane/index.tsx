52adbea66af70a0593765edfd4085009f0b85021
Syntax365 <synxtax365@gmail.com>
2022-12-10 16:07:06 -0500

    Update transition types

--- components/NavigationPane/index.tsx
+++ components/NavigationPane/index.tsx
@@ -53,6 +53,11 @@ export const NavigationPane = (props: any) => {
           className="w-full z-10  text-white flex text-center rounded-t-lg bottom-0 fixed"
           style={{ backgroundColor: "#301934" }}
           initial={{ height: "50px" }}
+          transition={{
+            y: { type: "spring", stiffness: 100 },
+            duration: 0.4,
+            delay: 0.1,
+          }}
           animate={isMobileOpen ? { height: "320px" } : {}}
         >
           <div className="w-full overflow-y-hidden items-center">
